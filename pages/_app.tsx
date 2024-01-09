69e1cecd8a5bc9fd018a57977789386c4f1f1bea
MOOD MNKY <simeon.bowman@moodmnky.com>
2024-01-09 09:07:52 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import { BubbleChat } from 'flowise-embed-react'; // Import BubbleChat
 
 import * as Fathom from 'fathom-client'
 // used for rendering equations (optional)
@@ -61,5 +62,51 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <BubbleChat
+        chatflowid="044b8e10-149c-4034-af54-d7d617dcd557"
+        apiHost="https://fortress-flowise.moodmnky.com"
+        theme={{
+          button: {
+            backgroundColor: "#724036",
+            right: 20,
+            bottom: 20,
+            size: "medium",
+            iconColor: "white",
+            customIconSrc: "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/svg/google-messages.svg",
+          },
+          chatWindow: {
+            welcomeMessage: "Welcome back Duncan! How can I be of service?",
+            backgroundColor: "#724036",
+            height: 700,
+            width: 400,
+            fontSize: 16,
+            poweredByTextColor: "#724036",
+            botMessage: {
+              backgroundColor: "#2F3437",
+              textColor: "#FFFFFF",
+              showAvatar: true,
+              avatarSrc: "https://cdn.discordapp.com/attachments/1083532452347269220/1194280879313403985/fortress_favicon_upscale_no_bg.png",
+            },
+            userMessage: {
+              backgroundColor: "#539997",
+              textColor: "#ffffff",
+              showAvatar: true,
+              avatarSrc: "https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/usericon.png",
+            },
+            textInput: {
+              placeholder: "Type your question",
+              backgroundColor: "#2F3437",
+              textColor: "#ffffff",
+              sendButtonColor: "#193F4E",
+            }
+          }
+        }}
+      />
+  <Component {...pageProps} />
+  </>
+  );
 }
+    
+
